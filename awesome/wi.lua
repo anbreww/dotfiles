1b425da65f65bd82be17eabeadcb5fe9b157a91d
Andrew Watson <andy@watsons.ch>
2010-03-12 21:06:39 +0100

    fixed a few bugs in widgets

--- awesome/wi.lua
+++ awesome/wi.lua
@@ -47,6 +47,10 @@ function get_loki()
         lcolour = beautiful.down_red
     end
 
+    if lcolour == nil then
+        lcolour = "#ffffff"
+    end
+
     loki_status_formatted = 'loki : <span color="' .. lcolour .. '">' .. loki_status .. '</span>'
 
     return {loki_status_formatted}
@@ -67,13 +71,14 @@ separator		= widget({ type = "imagebox" })
 spacer.text		= " "
 separator.image	= image(beautiful.widget_sep)
 -- }}}
---
--- Battery charge widget
+
+-- {{{ Battery charge widget
 baticon = widget({ type = "imagebox" })
 baticon.image = image(beautiful.widget_bat)
 
 batwidget = widget({ type = "textbox" })
 vicious.register(batwidget, vicious.widgets.bat, "$1$2%", 61, "BAT0")
+-- }}}
 
 -- Network usage widget
 netwidget = widget({ type = "textbox" })
@@ -128,7 +133,7 @@ cpugraph:set_gradient_colors({ beautiful.fg_end_widget,
 vicious.register(cpugraph,  vicious.widgets.cpu,     "$1")
 vicious.register(tzswidget, vicious.widgets.thermal, "$1C", 19, "thermal_zone0")
 -- }}}
---
+
 --- {{{ Available updates widget
 updatewidget = widget({ type = "textbox" })
 updateicon = widget({ type = "imagebox" })
@@ -153,14 +158,14 @@ function get_upg()
 end
 
 function popup_pac()
-    naughty.notify { text = get_upg(), timeout = 5, hover_timeout = 0.5, position = "top_right" }
+    naughty.notify { text = get_upg(), timeout = 5, hover_timeout = 0.5, position = "top_right", screen = screen.count() }
 end
 
 -- buttons
 updateicon:buttons(awful.util.table.join(awful.button({ }, 1, popup_pac)))
 updatewidget:buttons(awful.util.table.join(awful.button({ }, 1, popup_pac)))
 
---}}}
+-- --}}}
 --
 ipwidget = widget ({ type = "textbox" })
 vicious.register(ipwidget, get_ip, "remote: $1", 600)
@@ -168,10 +173,10 @@ localipwidget = widget ({ type = "textbox" })
 vicious.register(localipwidget, get_local_ip, "local: $1", 600)
 hostnamewidget = widget ({ type = "textbox" })
 vicious.register(hostnamewidget, get_hostname, "$1", 600)
-
+ 
 weatherwidget = widget ({ type = "textbox" })
 vicious.register(weatherwidget, get_weather, "$2, $1°C", 600)
-
+ 
 wmailwidget = widget ({ type = "textbox" })
 vicious.register(wmailwidget, get_wmail, "$1", 120)
 wmailicon = widget ({ type = "imagebox" })
@@ -219,40 +224,40 @@ vicious.register(membar, vicious.widgets.mem, "$1", 13)
 memtext = widget({ type = "textbox" })
 vicious.register(memtext, vicious.widgets.mem, "RAM: $1%", 9)
 
--- Memory details popup
-
--- }}}
-
--- {{{ File system usage
-fsicon = widget({ type = "imagebox" })
-fsicon.image = image(beautiful.widget_fs)
--- Initialize widgets
-fs = {
-  r = awful.widget.progressbar(),  h = awful.widget.progressbar(),
-  s = awful.widget.progressbar(),  b = awful.widget.progressbar()
-}
--- Progressbar properties
-for _, w in pairs(fs) do
-  w:set_width(5)
-  w:set_height(12)
-  w:set_vertical(true)
-  w:set_background_color(beautiful.fg_off_widget)
-  w:set_border_color(beautiful.border_widget)
-  w:set_color(beautiful.fg_widget)
-  w:set_gradient_colors({ beautiful.fg_widget,
-     beautiful.fg_center_widget, beautiful.fg_end_widget
-  }) -- Register buttons
-  w.widget:buttons(awful.util.table.join(
-    awful.button({ }, 1, function () exec("rox", false) end)
-  ))
-end -- Enable caching
-vicious.enable_caching(vicious.widgets.fs)
----- Register widgets
-vicious.register(fs.r, vicious.widgets.fs, "${/ used_p}",            599)
---vicious.register(fs.h, vicious.widgets.fs, "${/media/Elements used_p}", 599)
-if hostname == "fenrir" then
-	vicious.register(fs.h, vicious.widgets.fs, "${/home used_p}",599)
-elseif hostname == "heimdall" then
-	vicious.register(fs.h, vicious.widgets.fs, "${/media/photos used_p}", 599)
-end
+-- -- Memory details popup
+-- 
+-- -- }}}
+-- 
+-- -- {{{ File system usage
+-- fsicon = widget({ type = "imagebox" })
+-- fsicon.image = image(beautiful.widget_fs)
+-- -- Initialize widgets
+-- fs = {
+--   r = awful.widget.progressbar(),  h = awful.widget.progressbar(),
+--   s = awful.widget.progressbar(),  b = awful.widget.progressbar()
+-- }
+-- -- Progressbar properties
+-- for _, w in pairs(fs) do
+--   w:set_width(5)
+--   w:set_height(12)
+--   w:set_vertical(true)
+--   w:set_background_color(beautiful.fg_off_widget)
+--   w:set_border_color(beautiful.border_widget)
+--   w:set_color(beautiful.fg_widget)
+--   w:set_gradient_colors({ beautiful.fg_widget,
+--      beautiful.fg_center_widget, beautiful.fg_end_widget
+--   }) -- Register buttons
+--   w.widget:buttons(awful.util.table.join(
+--     awful.button({ }, 1, function () exec("rox", false) end)
+--   ))
+-- end -- Enable caching
+-- vicious.enable_caching(vicious.widgets.fs)
+-- ---- Register widgets
+-- vicious.register(fs.r, vicious.widgets.fs, "${/ used_p}",            599)
+-- --vicious.register(fs.h, vicious.widgets.fs, "${/media/Elements used_p}", 599)
+-- if hostname == "fenrir" then
+-- 	vicious.register(fs.h, vicious.widgets.fs, "${/home used_p}",599)
+-- elseif hostname == "heimdall" then
+-- 	--vicious.register(fs.h, vicious.widgets.fs, "${/media/photos used_p}", 599)
+-- end
 -- }}}
