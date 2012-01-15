a6e8e01c907309c3ee2dd355686dfd0e326ab6b9
Andrew Watson <andy@watsons.ch>
2012-01-15 16:44:37 +0100

    scripts: correct some network bugs

--- scripts/getlocalip.sh
+++ scripts/getlocalip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # get local IP address
-ifconfig|grep 'inet addr'|sed -n -e 's/.*addr://' -e 's/ .*$//p' | grep -v '127.0.0.1' || echo "not connected"
+ifconfig|grep 'inet'|sed -n -e 's/.*inet //' -e 's/ .*$//p' | grep -v '127.0.0.1' || echo "not connected"
